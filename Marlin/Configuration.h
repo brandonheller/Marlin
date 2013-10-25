e36e56d1fc60c7f442e2095ed18d59f401804d94
Brandon Heller <brandonh@stanford.edu>
2013-10-24 23:15:43 -0700

    New e-steps for minimal mk7 extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -357,7 +357,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 80, 523}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 80, 98.07}
 #define DEFAULT_MAX_FEEDRATE          {200, 200, 200, 200}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,9000,9000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
